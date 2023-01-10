d8760f27b1331eabf299da029681de597ca91f28
tedha012 <tedha012@gmail.com>
2023-01-10 11:03:40 +0900

    classmethod

---
+++ cls_mem_met.py
@@ -0,0 +1,21 @@
+class A:
+    c_mem = 10
+
+    @classmethod
+    def cls_f(cls):
+        print(cls.c_mem)
+
+    def __init__(self, num) -> None:
+        self.i_mem = num
+
+    def ins_f(self):
+        print(self.i_mem)
+
+
+if __name__ == "__main__":
+    print(A.c_mem)
+    A.cls_f
+
+    a = A(20)
+    print(a.c_mem)
+    a.ins_f()
